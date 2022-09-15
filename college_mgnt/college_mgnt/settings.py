4f35229c00e0c8cb910e9b07cda2e141552b4ff7
00Aayush00 <pokharelaayush2@gmail.com>
2022-09-15 08:49:48 +0545

    added database in project

--- college_mgnt/college_mgnt/settings.py
+++ college_mgnt/college_mgnt/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'college_mgnt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'student_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
